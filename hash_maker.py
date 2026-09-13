e721d4239b83414f0e00a84807e67f3c83f89e1d
Kent Chen <kent891026@gmail.com>
2026-09-13 12:44:44 +0800

    移除暫存檔

--- hash_maker.py
+++
@@ -1,9 +0,0 @@
-
-import bcrypt
-
-# boss_password = "1234"
-# admin_password = "admin888"
-my_password = "admin888"
-
-hashed = bcrypt.hashpw(my_password.encode('utf-8'), bcrypt.gensalt())
-print(f"Hashed password: {hashed.decode('utf-8')}")
